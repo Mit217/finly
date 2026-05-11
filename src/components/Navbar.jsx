812f48dafe716b7ad2f5757ce35844a1b138117f
Mit217 <mithooozzz@gmail.com>
2026-05-11 23:28:08 +0530

    initial dashboard

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import "../style.css";
+import {Link} from "react-router-dom";
+function Navbar(){
+    return(
+        <nav className="navbar">
+            <h2>Finly.</h2>
+
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/transactions">Transactions</Link>
+                <Link to="/budget">Budget</Link>
+                <Link to="/analytics">Analytics</Link>
+            </div>
+
+        </nav>
+    );
+}
+export default Navbar
